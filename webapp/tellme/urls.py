9cbd65259b545762ebb331b0385cc011676dc00f
Pinar Kaymaz <pinarkaymaz6@gmail.com>
2020-07-21 15:52:01 +0200

    Add input forms

--- webapp/tellme/urls.py
+++ webapp/tellme/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     #127.0.0.1/polls/2
     #path('<int:question_id>/results', views.results, name="results"),
     #127.0.0.1/polls/2/results
-    #path('<int:question_id>/vote', views.vote, name="vote"),
-    #127.0.0.1/polls/2/vote
+    path('<int:question_id>/add_answer', views.add_answer, name="add_answer"),
+    path('add_question', views.add_question, name="add_question"),
+
 ]
--- webapp/tellme/views.py
+++ webapp/tellme/views.py
@@ -3,14 +3,41 @@ from django.template import loader
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question
+from django.utils import timezone
 # Create your views here.
 
 def index(request):
+    print("INDEX")
     latest_questions = Question.objects.order_by('q_date')[:5]
     context = {'latest_questions':latest_questions}
     return render(request, 'tellme/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question,pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     context = {"question":question}
-    return  render(request, 'tellme/detail.html', context)
+    return render(request, 'tellme/detail.html', context)
+
+def add_question(request):
+    print("ADD QUESTION")
+    try:
+        q_title = request.POST['q_title']
+        q_text = request.POST['q_text']
+        print(q_title,q_text)
+        question = Question(q_title=q_title, q_text=q_text, q_date=timezone.now())
+        question.save()
+    except:
+        return render(request, 'tellme/index.html')
+
+    return HttpResponseRedirect(reverse('tellme:index')) 
+
+
+def add_answer(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        text = request.POST['answer']
+        question.answer_set.create(a_text=text, a_date=timezone.now())
+        question.save()
+    except:
+        return render(request, 'tellme/detail.html', {'question': question, 'error_message': 'Please enter some text'})
+
+    return HttpResponseRedirect(reverse('tellme:detail', args=(question.id,))) 
